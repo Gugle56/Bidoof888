62dd3ee43f97e268327e0f2460ea593ae0e8c9b7
Pomgle <chanotai52@gmail.com>
2026-05-20 18:59:02 +0700

    Update main.py

--- main.py
+++ main.py
@@ -182,7 +182,7 @@ def individual_bot_worker(account_index, token, channel_id):
         # =========================
 
         # พักปกติ 30-90 วินาที
-        sleep_time = random.randint(15, 60)
+        sleep_time = random.randint(15, 30)
 
         # มีโอกาส 10% พักยาว
         if random.randint(1, 10) == 1:
